afb76e99fbb5be278f8a1be0c01e6787163103af
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-27 22:00:00 +0300

    Refactor some of the code

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,13 +4,15 @@ public class GameFrame extends JFrame {
 
     GameFrame() {
 
-        this.add(new GamePanel());
-        this.setTitle("Snake");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.pack();
-        this.setVisible(true);
-        this.setLocationRelativeTo(null);
-
+        System.setProperty("java.awt.headless", "false");
+        SwingUtilities.invokeLater(() -> {
+            this.add(new GamePanel());
+            this.setTitle("Snake");
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setResizable(false);
+            this.pack();
+            this.setVisible(true);
+            this.setLocationRelativeTo(null);
+        });
     }
 }
