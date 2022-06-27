e239be23f4e0794903eacab4ab1c5c1822139006
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-27 18:00:27 +0300

    Adding multicolor snake - code is commented

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -62,6 +62,8 @@ public class GamePanel extends JPanel implements ActionListener {
                     g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 } else {
                     g.setColor(new Color(45, 180, 0));
+                    //Adding multicolor snake
+                    //g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
                     g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
             }
