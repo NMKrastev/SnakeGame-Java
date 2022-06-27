c9bca07aff8c0d3c3ef436a9d283346f473cf243
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-27 15:31:05 +0300

    Created GameFrame constructor class and initialized it in main class

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,5 +4,13 @@ public class GameFrame extends JFrame {
 
     GameFrame() {
 
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
     }
 }
