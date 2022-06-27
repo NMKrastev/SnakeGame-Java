d272d066b756b687706aaa88b97623f3e2c2906a
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-27 17:20:14 +0300

    Updated GamePanel.java so now the snake can move and also checks for collisions

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
@@ -48,7 +51,17 @@ public class GamePanel extends JPanel implements ActionListener {
             g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
         }
         g.setColor(Color.red);
-        g.fillOval(appleX, appleY, UNIT_SIZE,UNIT_SIZE);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for (int i = 0; i < initialBodyParts; i++) {
+            if (i == 0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            } else {
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
     }
 
     public void newApple() {
@@ -57,15 +70,60 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void move() {
+        for (int i = initialBodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
 
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
     }
 
     public void checkApple() {
-
+        if ((x[0] == appleX) && (y[0]) == appleY)) {
+            initialBodyParts++;
+        }
     }
 
     public void checkCollisions() {
+        //Checks if head collides with the body
+        for (int i = initialBodyParts; i > 0; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
+        }
+        //Checks if head touches left border
+        if (x[0] < 0) {
+            running = false;
+        }
+        //Checks if head touches right border
+        if (x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+        //Checks if head touches top border
+        if (y[0] < 0) {
+            running = false;
+        }
+        //Checks if head touches bottom border
+        if (y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
 
+        if (!running) {
+            timer.stop();
+        }
     }
 
     public void gameOver(Graphics g) {
@@ -74,13 +132,40 @@ public class GamePanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
 
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
 
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if (direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if (direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+            }
         }
     }
 
