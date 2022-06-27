3e62e8c1f649d7759985f609e1ed4b5e23df6135
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-27 22:05:56 +0300

    Refactor some of the code

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -3,9 +3,7 @@ import javax.swing.*;
 public class SnakeGame {
     public static void main(String[] args) {
 
-        System.setProperty("java.awt.headless", "false");
-        SwingUtilities.invokeLater(() -> {
             new GameFrame();
-        });
+
     }
 }
