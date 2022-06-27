156bfda4c7e02b8d85395f910e863ce388307f4c
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-27 16:31:24 +0300

    Updated GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -39,14 +39,21 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        draw(g);
     }
 
     public void draw(Graphics g) {
-
+        for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE,UNIT_SIZE);
     }
 
     public void newApple() {
-
+        appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
     }
 
     public void move() {
