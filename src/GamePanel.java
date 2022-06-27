2db024c57cf106e42f4ca4b0290269a7e499380c
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-27 17:56:17 +0300

    Updated GamePanel.java - removed the grid

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -48,10 +48,10 @@ public class GamePanel extends JPanel implements ActionListener {
     public void draw(Graphics g) {
 
         if (running) {
-            for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+            /*for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
                 g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
                 g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
-            }
+            }*/
             g.setColor(Color.red);
             g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
